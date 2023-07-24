8d41d3cbba4218b4fa7087fb4e4f44f36a7277cf
Haakan T Johansson <f96hajo@chalmers.se>
2023-07-24 22:13:32 +0200

    Example from S245 .root file, plots from Thomas' root manual.

---
+++ example/s245/s245.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+r10_proton_root=r10_470-529_beam_proton_hit_track.root
+
+r10_frag_root=r10_53x_beam_frag_hit_track.root
+
+[ -e $r10_proton_root ] || \
+    wget http://fy.chalmers.se/subatom/advsubdet/$r10_proton_root
+
+[ -e $r10_frag_root ] || \
+    wget http://fy.chalmers.se/subatom/advsubdet/$r10_frag_root
+
+plutt=../../plutt
+
+$plutt -f r10_proton.plutt -r h509 $r10_proton_root &
+
+$plutt -f r10_frag.plutt -r h509 $r10_frag_root &
+
