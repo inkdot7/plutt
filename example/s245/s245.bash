de4972428c29c310c912d559ed742d25c41d8363
Hans Toshihide Törnqvist <hans.tornqvist@chalmers.se>
2023-07-25 13:44:44 +0200

    Added "pathing" to the s245 example.

--- example/s245/s245.bash
+++ example/s245/s245.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname -- "${BASH_SOURCE[0]}")
+
 r10_proton_root=r10_470-529_beam_proton_hit_track.root
 
 r10_frag_root=r10_53x_beam_frag_hit_track.root
@@ -15,4 +17,3 @@ plutt=../../plutt
 $plutt -f r10_proton.plutt -r h509 $r10_proton_root &
 
 $plutt -f r10_frag.plutt -r h509 $r10_frag_root &
-
